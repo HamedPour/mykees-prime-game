43d9112be791f1d651a8c38ed5d8a33834b6f9a7
HamedPour <hamed_k.pour@hotmail.com>
2020-07-20 13:32:32 +0100

    Updates

---
+++ precache-manifest.9e3b8b2a1d84468312a7197157ef013f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3945d5be9d14d18445509873dfc91d",
+    "url": "/mykees-prime-game/index.html"
+  },
+  {
+    "revision": "929395dc7128d82bc715",
+    "url": "/mykees-prime-game/static/css/main.3ff6c8c9.chunk.css"
+  },
+  {
+    "revision": "5a66cef7fb109eeaf00c",
+    "url": "/mykees-prime-game/static/js/2.a18c83a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mykees-prime-game/static/js/2.a18c83a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929395dc7128d82bc715",
+    "url": "/mykees-prime-game/static/js/main.d6add8a5.chunk.js"
+  },
+  {
+    "revision": "6f6e956f50ea053c2d26",
+    "url": "/mykees-prime-game/static/js/runtime-main.2402494a.js"
+  }
+]);
